63d829078e87c9c3b167d4c800588126dd917f92
Vũ Trí Dũng <163394310+VuTriDung@users.noreply.github.com>
2025-02-24 23:47:12 +0700

    Add files via upload

---
+++ Mid_Exam_Ex3/Mid_Exam_Ex3/Mid_Exam_Ex3.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int thuaSo[100];
+int dem = 0;
+
+void PhanTichSo(int N, int k) {
+    if (N == 1) {
+        // In kết quả khi N = 1
+        for (int i = 0; i < dem; i++) {
+            if (i > 0) cout << "*";
+            cout << thuaSo[i];
+        }
+        cout << endl;
+        exit(0);
+    }
+
+    bool found = false; // Đánh dấu để kiểm tra xem có tìm được thừa số không
+    for (int i = k; i <= sqrt(N); i++) {
+        if (N % i == 0) {   
+            thuaSo[dem++] = i;
+            PhanTichSo(N / i, i);
+            dem--;
+            found = true; // Đánh dấu là tìm được thừa số
+        }
+    }
+
+    // Nếu không tìm thấy thừa số nào từ 2 đến sqrt(N), thì N là số nguyên tố
+    if (!found && N > 1) {
+        thuaSo[dem++] = N;
+        for (int i = 0; i < dem; i++) {
+            if (i > 0) cout << "*";
+            cout << thuaSo[i];
+        }
+        cout << endl;
+        exit(0);
+    }
+}
+
+int main() {
+    int N;
+    cout << "Nhap so N: ";
+    cin >> N;
+
+    cout << "Cac thua so nguyen to cua " << N << " la: ";
+    PhanTichSo(N, 2);
+
+    return 0;
+}
